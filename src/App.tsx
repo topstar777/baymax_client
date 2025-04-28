089309121581bad2874d26c3c1b5328936b3e9a3
Thomas Hacker <thomashacker777@gmail.com>
2025-04-28 00:22:00 -0500

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,27 @@ import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { io } from "socket.io-client";
 import MainLayout from "./components/MainLayout";
 import NavBar from "./components/NavBar";
+import SignUpPage from "./components/SignUpPage";
 import LoginPage from "./components/LoginPage";
 import "./App.css";
 
 const CHAT_PORT = "general-chat";
 const OPENAI_PORT = "ai-chat";
-const SERVER_URL = "http://13.57.226.132:5001";
+const VOICE_PORT = "voice-share";
+const SERVER_URL = "https://baymax-backend-l2rs.onrender.com";
 
 const VITE_CHAT_URL = `${SERVER_URL}/${CHAT_PORT}`;
 const VITE_OPENAI_URL = `${SERVER_URL}/${OPENAI_PORT}`;
+const VITE_VOICE_URL = `${SERVER_URL}/${VOICE_PORT}`;
 
 const socketChat = io(VITE_CHAT_URL);
 const socketOpenAI = io(VITE_OPENAI_URL);
+const socketVoice = io(VITE_VOICE_URL);
 
 const App = ({ toggleTheme }: { toggleTheme: () => void }) => {
   const location = useLocation();
-  const hideNavbar = location.pathname === "/";
+  const hideNavbar =
+    location.pathname === "/" || location.pathname === "/signup";
 
   return (
     <>
@@ -29,10 +34,15 @@ const App = ({ toggleTheme }: { toggleTheme: () => void }) => {
             <LoginPage socket={socketChat} openAiSocket={socketOpenAI} />
           }
         />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route
           path="/chat"
           element={
-            <MainLayout socket={socketChat} openAiSocket={socketOpenAI} />
+            <MainLayout
+              socket={socketChat}
+              openAiSocket={socketOpenAI}
+              voiceSocket={socketVoice}
+            />
           }
         />
         <Route path="*" element={<Navigate to="/" />} />
